2b417f2be366143e8a59f91b202649b5551d7632
Mathilde Arconte <marconte@xprdev.fr>
2018-04-15 03:47:03 +0200

    Contact Section

--- front-page.php
+++ front-page.php
@@ -11,17 +11,68 @@
  *
  * @package marconte
  */
+$contact_title = get_field('contact_title', 'option');
+$contact_form = get_field('contact_form', 'option');
+$contact_background = get_field('contact_background', 'option');
+
+
 
 get_header(); ?>
 <div id="primary" class="content-area">
     <main id="main" class="site-main" role="main">
         <div id="fullpage" class="wrapper">
 
-            <div id="section-home"class="section">Home</div>
-            <div id="section-services"class="section">Services</div>
-            <div id="section-portfolio"class="section">Portfolio</div>
-            <div id="section-about"class="section">About</div>
-            <div id="section-contact"class="section">Contact</div>
+            <section id="section-home"class="section">Home</section>
+            <section id="section-services"class="section">Services</section>
+            <section id="section-portfolio"class="section">Portfolio</section>
+            <section id="section-about"class="section">About</section>
+            <section id="section-contact"class="section" style="background-image: url('<?php echo $contact_background['url']; ?>')">
+            <div class="d-flex section_title">
+                <h2 class="vertical-text align-self-center" >Contact</h2>
+            </div>
+                <div class="row">
+                    <div class="col-sm-4">
+
+                    </div>
+                    <div class="col-sm-8 ">
+                        <div class="row">
+                            <div class="col-sm-4  contact_social d-flex justify-content-center">
+                                <ul class="row">
+                                <?php
+
+                                // check if the repeater field has rows of data
+                                if( have_rows('social', 'option') ):
+                                
+                                     // loop through the rows of data
+                                    while ( have_rows('social', 'option') ) : the_row(); ?>
+                                
+                                       <li class=" col-md-12 col-4 d-flex align-items-center p-3">
+                                       <span class="social-icon mr-2" > <?= the_sub_field('icon'); ?></span>
+                                        <a href=" <?= the_sub_field('link'); ?>"><?= the_sub_field('text'); ?></a>
+
+                                       </li>
+                                
+                                  <?php  endwhile;
+                                
+                                else :
+                                
+                                    // no rows found
+                                
+                                endif;
+                                
+                                ?>
+                                </ul>
+                            </div>
+                            <div class="col-sm-8 contact_form d-flex justify-content-center">
+                                <h3 class="text-center contact_title"><?= $contact_title?></h3>
+                            <?php echo do_shortcode( $contact_form ); ?>
+                            </div>
+                        </div>
+                    
+                    </div>
+                </div>
+            
+            </section>
         </div>
     </main>
 </div><!-- #primary -->
--- header.php
+++ header.php
@@ -10,15 +10,16 @@
  */
 
 ?><!DOCTYPE html>
-<html <?php language_attributes(); ?>>
+<html <?php language_attributes(); ?> style="	margin-top: 0 !important;">
 <head>
 <meta charset="<?php bloginfo( 'charset' ); ?>">
 <meta name="viewport" content="width=device-width, initial-scale=1">
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <script src="https://code.jquery.com/jquery-3.1.1.min.js" integrity="sha256-hVVnYaiADRTO2PzUGmuLJr8BLUSjGIZsDYGmIJLv2b8=" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/jquery.fullpage.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"></script>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js"></script>
+<script defer src="https://use.fontawesome.com/releases/v5.0.10/js/all.js" integrity="sha384-slN8GvtUJGnv6ca26v8EzVaR9DC58QEwsIk9q1QXdCU8Yu8ck/tL/5szYlBbqmS+" crossorigin="anonymous"></script>
+<!-- <script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/scrolloverflow.min.js"></script>
+ --><script src="https://cdnjs.cloudflare.com/ajax/libs/fullPage.js/2.9.7/vendors/jquery.easings.min.js"></script>
 <?php wp_head(); ?>
 </head>
 
